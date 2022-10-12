d1e9de3b0dee94a765ac982856db25cfe0e63d49
roto93 <roto93@yahoo.com.tw>
2022-10-13 00:52:43 +0800

    :zap: show metamask account using web3js

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import './css/App.css'
+import Web3 from 'web3'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [account, setAccount] = useState('');
+
+  const onAccountInputChange = (e) => {
+    setAccount(e.target.value)
+  }
+
+  useEffect(() => {
+    const GANACHE_URL = 'http://localhost:8545'
+    const web3 = new Web3(Web3.givenProvider || GANACHE_URL)
+
+    const loadAccount = async () => {
+      const accounts = await web3.eth.requestAccounts()
+
+      setAccount(accounts[0])
+    }
+
+    loadAccount()
+  }, [])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='header'>
+        <h2>My First Web3 </h2>
+        <p className="account">Account: {account}</p>
+        <input type="text" className="account__input" onChange={onAccountInputChange} />
+      </header>
     </div>
   )
 }
