69ee3d1498fd317af1314ba963294242011d6a7b
roto93 <roto93@yahoo.com.tw>
2022-10-13 11:43:25 +0800

    :zap: show network used and balance using web3js

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Web3 from 'web3'
 
 function App() {
   const [account, setAccount] = useState('');
+  const [network, setNetwork] = useState('');
+  const [balance, setBalance] = useState(0);
 
   const onAccountInputChange = (e) => {
     setAccount(e.target.value)
@@ -19,15 +21,25 @@ function App() {
       setAccount(accounts[0])
     }
 
-    loadAccount()
-  }, [])
+    const loadBalance = async () => {
+      const network = await web3.eth.net.getNetworkType()
+      const balance = await web3.eth.getBalance(account) // given in Wei
+      setNetwork(network)
+      console.log(network)
+      setBalance(balance)
+    }
+    if (!account) {
+      loadAccount()
+    } else loadBalance()
+  }, [account])
 
   return (
     <div className="App">
       <header className='header'>
         <h2>My First Web3 </h2>
-        <p className="account">Account: {account}</p>
-        <input type="text" className="account__input" onChange={onAccountInputChange} />
+        <p className="account">My Account: {account}</p>
+        <p>The Network used: {network}</p>
+        <p>My Balance: {balance / 1000000000} GWei</p>
       </header>
     </div>
   )
